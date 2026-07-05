f9839721602fa506b2acee60a4014d091bb3cc58
sharonsteinkamp <sharonsteinkamp390@gmail.com>
2026-07-04 21:58:21 -0300

    Add files via upload

---
+++ trabajo practico 4 info.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	float calificacion;
+	
+		printf("ingrese la calificacion: ");
+		scanf("%f", &calificacion);
+	
+	if (calificacion >= 90) {
+		printf("Calificacion = A\n");
+	} 
+	else if (calificacion >= 80 && calificacion < 90) {
+	
+		printf("Calificacion = B\n");
+	} 
+	else if (calificacion >= 70 && calificacion < 80) {
+		
+		printf("Calificacion = C\n");
+	} 
+	else if (calificacion >= 60 && calificacion < 70) {
+	
+		printf("Calificacion = D\n");
+	} 
+	else if (calificacion < 60) {
+		
+		printf("Calificacion = F\n");
+	}
+	
+	return 0;
+}
+
